973eb26c0a47a57bdb0d2c65885a953ba35c8772
vikashraj144 <vikashraj144@gmail.com>
2018-03-18 11:54:37 +0530

    function for Creating a new Note

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -1,19 +1,19 @@
-module.exports = function(app) {
+var Note = require('../models/note.model.js');
 
-    var notes = require('../controllers/note.controller.js');
+exports.create = function(req, res) {
+    // Create and Save a new Note
+    if(!req.body.content) {
+        return res.status(400).send({message: "Note can not be empty"});
+    }
 
-    // Create a new Note
-    app.post('/notes', notes.create);
+    var note = new Note({title: req.body.title || "Untitled Note", content: req.body.content});
 
-    // Retrieve all Notes
-    app.get('/notes', notes.findAll);
-
-    // Retrieve a single Note with noteId
-    app.get('/notes/:noteId', notes.findOne);
-
-    // Update a Note with noteId
-    app.put('/notes/:noteId', notes.update);
-
-    // Delete a Note with noteId
-    app.delete('/notes/:noteId', notes.delete);
-}
+    note.save(function(err, data) {
+        if(err) {
+            console.log(err);
+            res.status(500).send({message: "Some error occurred while creating the Note."});
+        } else {
+            res.send(data);
+        }
+    });
+};
