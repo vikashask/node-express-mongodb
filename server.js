f170697fc09f1e46c6e9fc09abb178cf222b04f0
vikashraj144 <vikashraj144@gmail.com>
2018-03-18 08:49:36 +0530

    configure database

--- server.js
+++ server.js
@@ -10,6 +10,25 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
+// Configuring the database
+var dbConfig = require('./config/database.config.js');
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(dbConfig.url, {
+    useMongoClient: true
+});
+
+mongoose.connection.on('error', function() {
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+});
+
+mongoose.connection.once('open', function() {
+    console.log("Successfully connected to the database");
+})
+
 // define a simple route
 app.get('/', function(req, res){
     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
